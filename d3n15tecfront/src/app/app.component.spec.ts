85cd436497178d6241fc0c0e6db995ad8780f7a7
Denis Henrique Pacheco da Costa <denishpcinfo@gmail.com>
2024-10-25 23:39:11 -0300

    ajuste bibliotecas

--- d3n15tecfront/src/app/app.component.spec.ts
+++ d3n15tecfront/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'food-delivery-app'`, () => {
+  it(`should have as title 'd3n15tecfront'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('food-delivery-app');
+    expect(app.title).toEqual('d3n15tecfront');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('food-delivery-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('d3n15tecfront app is running!');
   });
 });
