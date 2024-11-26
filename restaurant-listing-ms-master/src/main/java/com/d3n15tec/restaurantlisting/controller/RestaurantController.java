e953735b5132bb829d3b9f3c52de177a3983119d
Denis Henrique Pacheco da Costa <denishpcinfo@gmail.com>
2024-11-25 22:28:39 -0300

    ajustes busca id

--- restaurant-listing-ms-master/src/main/java/com/d3n15tec/restaurantlisting/controller/RestaurantController.java
+++ restaurant-listing-ms-master/src/main/java/com/d3n15tec/restaurantlisting/controller/RestaurantController.java
@@ -1,6 +1,5 @@
 package com.d3n15tec.restaurantlisting.controller;
 
-import com.d3n15tec.restaurantlisting.dto.RestaurantDTO;
 import com.d3n15tec.restaurantlisting.entity.Restaurant;
 import com.d3n15tec.restaurantlisting.service.RestaurantService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.List;
-import java.util.HashMap;
+import java.util.*;
 
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.PageRequest;
@@ -25,21 +21,15 @@ public class RestaurantController {
     @Autowired
     RestaurantService restaurantService;
 
-
     @GetMapping("/buscar-restaurantes")
-    public ResponseEntity<List<RestaurantDTO>> fetchAllRestaurants(){
-        List<RestaurantDTO> allRestaurants = restaurantService.findAllRestaurants();
+    public ResponseEntity<List<Restaurant>> fetchAllRestaurants(){
+        List<Restaurant> allRestaurants = restaurantService.findAllRestaurants();
         return new ResponseEntity<>(allRestaurants, HttpStatus.OK);
     }
 
-    @PostMapping("/add-restaurante")
-    public ResponseEntity<RestaurantDTO> saveRestaurant(@RequestBody RestaurantDTO restaurantDTO) {
-        RestaurantDTO restaurantAdded = restaurantService.addRestaurantInDB(restaurantDTO);
-        return new ResponseEntity<>(restaurantAdded, HttpStatus.CREATED);
-    }
 
     @GetMapping("/buscar-por-id/{id}")
-    public ResponseEntity<RestaurantDTO> findRestaurantById(@PathVariable Integer id) {
+    public Optional<Restaurant> findRestaurantById(@PathVariable Integer id) {
        return restaurantService.fetchRestaurantById(id);
     }
 
--- restaurant-listing-ms-master/src/main/java/com/d3n15tec/restaurantlisting/service/RestaurantService.java
+++ restaurant-listing-ms-master/src/main/java/com/d3n15tec/restaurantlisting/service/RestaurantService.java
@@ -1,19 +1,14 @@
 package com.d3n15tec.restaurantlisting.service;
 
-import com.d3n15tec.restaurantlisting.dto.RestaurantDTO;
 import com.d3n15tec.restaurantlisting.entity.Restaurant;
-import com.d3n15tec.restaurantlisting.mapper.RestaurantMapper;
 import com.d3n15tec.restaurantlisting.repo.RestaurantRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class RestaurantService {
@@ -21,10 +16,9 @@ public class RestaurantService {
     @Autowired
     RestaurantRepo restaurantRepo;
 
-    public List<RestaurantDTO> findAllRestaurants() {
+    public List<Restaurant> findAllRestaurants() {
         List<Restaurant> restaurants = restaurantRepo.findAll();
-        List<RestaurantDTO> restaurantDTOList = restaurants.stream().map(RestaurantMapper.INSTANCE::mapRestaurantToRestaurantDTO).collect(Collectors.toList());
-        return restaurantDTOList;
+        return restaurants;
     }
 
     public List<Restaurant> findAllRestaurantsSlaider() {
@@ -36,13 +30,8 @@ public class RestaurantService {
         return restaurantRepo.findAll(pageable);
     }
 
-    public RestaurantDTO addRestaurantInDB(RestaurantDTO restaurantDTO) {
-        Restaurant savedRestaurant =  restaurantRepo.save(RestaurantMapper.INSTANCE.mapRestaurantDTOToRestaurant(restaurantDTO));
-        return RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(savedRestaurant);
-    }
-
-    public ResponseEntity<RestaurantDTO> fetchRestaurantById(Integer id) {
+    public Optional<Restaurant> fetchRestaurantById(Integer id) {
         Optional<Restaurant> restaurant =  restaurantRepo.findById(id);
-        return restaurant.map(value -> new ResponseEntity<>(RestaurantMapper.INSTANCE.mapRestaurantToRestaurantDTO(value), HttpStatus.OK)).orElseGet(() -> new ResponseEntity<>(null, HttpStatus.NOT_FOUND));
+        return restaurant;
     }
 }
